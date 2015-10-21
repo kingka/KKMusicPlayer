adbe7a2af6dc8531c7f071c0b40af090d03570a6
Imanol <jk199054@163.com>
2015-10-21 14:53:08 +0800

    create project

---
+++ KKMusicPlayer/KKMusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KKMusicPlayer
+//
+//  Created by Imanol on 15/10/21.
+//  Copyright (c) 2015年 Imanol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KKMusicPlayer/KKMusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KKMusicPlayer
+//
+//  Created by Imanol on 15/10/21.
+//  Copyright (c) 2015年 Imanol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
